5ff4bcb8656696302cd72d04a24f9c6b2ffa86b4
santana-devin <fabriciosantana.devin@gmail.com>
2022-06-02 15:08:13 -0300

    Aplicativo Comcluido

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React,{useState,  useEffect} from 'react';
+import {View,StyleSheet,Image,TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import Shake from 'react-native-shake';
+
+const App = () => {
+  const [toogle,setToogle]  = useState(false);
+
+  const handleOnPressToogle = () => setToogle(oldToogle=>!oldToogle);
+   
+  useEffect(()=>{
+    Torch.switchState(toogle);
+  },[toogle]);
+
+  useEffect(()=>{
+    const sbscription = Shake.addListener(()=>{
+      setToogle(oldToogle=>oldToogle);
+    });
+
+
+    return ()=> sbscription.remove();
+  },[]);
+
+  return (
+  <View style={toogle?style.container:style.containerLight} >
+    <TouchableOpacity onPress={handleOnPressToogle}>
+      <Image 
+        style={[style.image, toogle?style.imageLamp:style.imageLampLight]}
+        source={toogle?require('./../assets/icons/eco-light.png'):require('./../assets/icons/eco-light-off.png')}
+        />
+      <Image 
+        style={[style.image, toogle?style.imageDio:style.imageDioLight]}
+        source={toogle?require('./../assets/icons/logo-dio.png'):require('./../assets/icons/logo-dio-white.png')}
+        />
+    </TouchableOpacity>
+  </View>
+)};
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image:{
+    alignSelf: 'center',
+    resizeMode: 'contain',    
+  },
+  imageLamp:{
+    width: 150,
+    height: 150,
+  },
+  imageDio:{
+    width: 250,
+    height: 250,
+  },
+  imageLampLight:{
+    width: 150,
+    height: 150,
+    tintColor:'white',
+  },
+  imageDioLight:{
+    width: 250,
+    height: 250,
+    tintColor:'white',
+  }
+});
